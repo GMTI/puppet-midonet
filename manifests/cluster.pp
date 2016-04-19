4c8c42bb79aca3deff3abc44d3197291f1603f33
Jim Hayner <jhayner@gannett.com>
2016-04-19 20:36:47 +0000

    revert order changes and force order

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -6,7 +6,15 @@ class midonet::cluster(
 {
 
   $zk_servers = regsubst($zk_server_list, '$', ':2181')
-  
+
+  package {'midonet-cluster':
+    ensure  => present,
+  } ->
+
+  package {'midonet-tools':
+    ensure  => present,
+  } ->
+
   file {'/etc/midonet/midonet.conf':
     owner   => 'root',
     group   => 'root',
@@ -16,43 +24,35 @@ class midonet::cluster(
     require => Package['midonet-cluster'],
   } ->
 
-  package {'midonet-cluster':
-    ensure  => present,
-  } ->
-
-  package {'midonet-tools':
-    ensure  => present,
-  }
-
   exec {'mn-conf-metadata-url':
     command => "mn-conf set -t default 'agent.openstack.metadata.nova_metadata_url : \"http://${management_vip}:8775\"'",
     path    => '/usr/bin:/bin',
     require => Package['midonet-tools'],
-  }
+  } ->
 
   exec {'mn-conf-metadata-secret':
     command => "mn-conf set -t default 'agent.openstack.metadata.shared_secret : ${shared_secret}'",
     path    => '/usr/bin:/bin',
     require => Package['midonet-tools'],
-  }
+  } ->
 
   exec {'mn-conf-metadata-state':
     command => "mn-conf set -t default 'agent.openstack.metadata.enabled : true'",
     path    => '/usr/bin:/bin',
     require => Package['midonet-tools'],
-  }
+  } ->
 
   exec {'mn-conf-replication':
     command => "mn-conf set -t default 'cassandra.replication_factor : 3'",
     path    => '/usr/bin:/bin',
     require => Package['midonet-tools'],
-  }
+  } ->
 
   exec {'mn-conf-keystone-admin':
     command => "/bin/echo -e 'cluster.auth {\n    provider_class = \"org.midonet.cluster.auth.keystone.KeystoneService\"\n    admin_role = \"admin\"\n    keystone.tenant_name = \"admin\"\n    keystone.admin_token = \"${keystone_admin_token}\"\n    keystone.host = ${management_vip}\n    keystone.port = 35357\n}' | mn-conf set -t default",
     path    => '/usr/bin:/bin',
     require => Package['midonet-tools'],
-  }
+  } ->
 
   service {'midonet-cluster':
     ensure  => running,
